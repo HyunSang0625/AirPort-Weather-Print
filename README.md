48f9c023892ba0079323d01e79447521643cd131
HyunSang Park <parkhyunsang@kakao.com>
2020-05-12 19:36:49 +0900

    Update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 비행 시뮬레이션을 하시나요? 아니면 파일럿 이신가요?  
 안전한 비행을 위해서 날씨를 프린터기를 출력합니다.  
+
+## 사용 가능한 시뮬레이션
+- X-Plane 
+- Microsoft Flight Simulator X
+- Prepar3D
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pdfkit
 import json
 import xmltodict
-import xml.etree.ElementTree as parse
+import pandas as  pd
+import urllib.request as link
+from xml.etree.ElementTree import parse
+import os
 import datetime
 
 rksi = "RKSI"
@@ -27,12 +30,16 @@ date = datetime.datetime.now()
 waring = "안녕하세요! Metar 자동 프린트 시스템입니다.\n 본 시스템은 항공기상청 API를 이용하여서 불러옵니다."
 error_waring = "Error : ICAO Code entered incorrectly | 다시 입력하세요."
 
-# Airport Input ICAOgit push -u origin master
-
 airport = input("ICAO Code Capital Letter : ")
 
 if (airport == rksi):
-    print("RKSI / 인천국제공항")
+    print("RKSI / 인천국제공항 | 인천광역시 중구 운서동")
+    # Inculde API 
+    response = link.urlopen(airport_api + rksi).read()
+    xmldoc = parse(response)
+    root = xmldoc.getroot()
+    main_print = root.findtext('metarMsg')
+    print(main_print)
     print("Lookup Time(조회시간) : ", date)
 else:
     print(error_waring)    
